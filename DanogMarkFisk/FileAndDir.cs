3102c01715b732e633e007e143b0876911e19af9
Mark-PC <Mark-PC@DESKTOP-FBC0J6Q>
2023-01-26 12:19:14 +0100

    Mark was Here

--- DanogMarkFisk/FileAndDir.cs
+++ DanogMarkFisk/FileAndDir.cs
@@ -13,5 +13,19 @@ namespace DanogMarkFisk
         private string aq2 = "@C:/User/Dan&Mark/Aq2";
         private string aq3 = "@C:/User/Dan&Mark/Aq3";
         private string aq4 = "@C:/User/Dan&Mark/Aq4";
+
+
+        public void Control()
+        {
+
+            if(!Directory.Exists(maindir))
+            {
+            Directory.CreateDirectory(maindir);
+            Directory.CreateDirectory(aq1);
+            Directory.CreateDirectory(aq2);
+            Directory.CreateDirectory(aq3);
+                Directory.CreateDirectory(aq4);
+            }
+        }
     }
 }
